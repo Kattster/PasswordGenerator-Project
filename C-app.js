ac968454ec67490b8544e62f2e32bee6edbf4e89
“Kattster” <katt@katsite.com>
2022-04-13 11:08:27 -0500

    Adding PasswordGeneratorProject

---
+++ C-app.js
@@ -0,0 +1,103 @@
+console.log("HELLO YOU BEAUTIFUL WORLD YOU");
+
+function randomIndex(str){
+    return Math.floor(Math.random() * str.length);
+}
+
+function getRandomLower(){
+    const letters = `abcdefghijklmnopqrstuvwxyz`;
+    return letters[randomIndex(letters)];
+}
+
+function getRandomUpper(){
+    const letter = getRandomLower();
+    return letter.toUpperCase();
+}
+
+function getRandomNumber(){
+    const numbers = `0123456789`;
+    return numbers[randomIndex(numbers)];
+}
+
+function getRandomSymbol(){
+    const symbol = `!@#$%^&*(){}[]=<>/,.`;
+    return symbol[randomIndex(symbol)]; 
+}
+
+const randomFunctions = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber, 
+    symbol: getRandomSymbol
+};
+
+const resultEl = document.querySelector(`#result`);
+const clipboardEl = document.querySelector(`#clipboard`);
+const lowercaseEl = document.querySelector(`#lowercase`);
+const uppercaseEl = document.querySelector(`#uppercase`);
+const numbersEl = document.querySelector(`#numbers`);
+const symbolsEl = document.querySelector(`#symbols`);
+const lengthEl = document.querySelector(`#length`);
+const generateEl = document.querySelector(`#generate`);
+
+function generatePassword(lower, upper, number, symbol, length){
+    let generatedPassword = ``;
+    const typesCount = lower + upper + number + symbol;
+
+    if(typesCount === 0){
+        alert(`Please select at least one option`);
+        return ``;
+    }
+
+    let typesArr = [
+        [`lower`, lower],
+        [`upper`, upper],
+        [`number`, number],
+        [`symbol`, symbol]
+    ];
+
+    typesArr = typesArr.filter(item => {
+        console.log(item[1]);
+        return item[1];
+    });
+    
+    for (i = 0; i < length; i += typesCount){
+        typesArr.forEach(type => {
+            const funcName = type[0];
+            generatedPassword += randomFunctions[funcName]();
+        });
+    }
+    
+    const finalPassword = generatedPassword.slice(0, length);
+    return finalPassword;
+}
+
+generateEl.addEventListener(`click`, () => {
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+    const length = parseInt(lengthEl.value);
+
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+clipboardEl.addEventListener(`click`, () => {
+    const textarea = document.createElement(`textarea`);
+    const password = resultEl.innerText;
+    const body = document.querySelector(`body`);
+
+    if (password === ``){
+        alert(`Please generate a password first`);
+        return;
+    }
+
+    textarea.value = password;
+
+    body.append(textarea);
+    textarea.select();
+    document.execCommand(`copy`);
+    textarea.remove();
+
+    alert(`Password has been copied to the clipboard`);
+})
